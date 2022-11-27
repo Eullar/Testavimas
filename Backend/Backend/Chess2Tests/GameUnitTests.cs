c56d55ca595f272d475a430fd1d8054fae11002c
Dominykas <dominykas.petkevicius@gmail.com>
2022-11-27 16:53:08 +0200

    +1 test

--- Backend/Backend/Chess2Tests/GameUnitTests.cs
+++ Backend/Backend/Chess2Tests/GameUnitTests.cs
@@ -13,6 +13,12 @@ public class Tests
         _game = Game.GetGameInstance();
     }
 
+    [Dispose]
+    public void Dispose()
+    {
+        
+    }
+
     [Test]
     public void AddPlayer_PlayerWithNameAlreadyExists_ReturnsCorrectEnum()
     {
@@ -20,11 +26,40 @@ public class Tests
         var player = new Player("some connection id", "PlayerBeingTested", Color.Blue, new List<Unit>());
         var initialResult = _game.AddPlayer(player);
         Assert.That(initialResult, Is.EqualTo(AddPlayerState.Completed), "Could not add intial player");
-        
+
         // Act
         var result = _game.AddPlayer(player);
 
         // Assert
         Assert.That(result, Is.EqualTo(AddPlayerState.PlayerWithNameExists));
+    }
+
+    [Test]
+    public void AddPlayer_ServerIsFull_ReturnsCorrectEnum()
+    {
+        // Arrange
+        var player1 = new Player("some connection id1", "PlayerBeingTested1", Color.Blue, new List<Unit>());
+        var player2 = new Player("some connection id2", "PlayerBeingTested2", Color.Red, new List<Unit>());
+        var player3 = new Player("some connection id3", "PlayerBeingTested3", Color.Yellow, new List<Unit>());
+        var player4 = new Player("some connection id4", "PlayerBeingTested4", Color.Green, new List<Unit>());
+        var player5 = new Player("some connection id5", "PlayerBeingTested5", Color.Green, new List<Unit>());
+
+        var initialResult1 = _game.AddPlayer(player1);
+        Assert.That(initialResult1, Is.EqualTo(AddPlayerState.Completed), "Could not add intial player 1");
+
+        var initialResult2 = _game.AddPlayer(player2);
+        Assert.That(initialResult2, Is.EqualTo(AddPlayerState.Completed), "Could not add intial player 2");
+
+        var initialResult3 = _game.AddPlayer(player3);
+        Assert.That(initialResult3, Is.EqualTo(AddPlayerState.Completed), "Could not add intial player 3");
+
+        var initialResult4 = _game.AddPlayer(player4);
+        Assert.That(initialResult4, Is.EqualTo(AddPlayerState.Completed), "Could not add intial player 4");
+
+        // Act
+        var result = _game.AddPlayer(player5);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(AddPlayerState.ServerIsFull));
     }
 }
